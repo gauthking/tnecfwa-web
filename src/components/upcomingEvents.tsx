918eab3d9ff595383375e37cfd3b5acab0837f2f
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2025-10-26 23:12:55 +0530

    remove unused poster import in UpcomingEvents component

--- src/components/upcomingEvents.tsx
+++ src/components/upcomingEvents.tsx
@@ -1,6 +1,5 @@
 // import poster from "../assets/upcomingevent.png";
 import poster2 from "../assets/upcomingevents5.jpg";
-import poster1 from "../assets/upcomingevent3.jpg";
 
 const UpcomingEvents = () => {
   return (
